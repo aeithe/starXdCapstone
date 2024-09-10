fd35816d89359ec6a97e6a99636d91b27f9de867
Arisa <pisareva.alisa@gmail.com>
2024-09-10 18:47:13 -0400

    added react elements woohoo

---
+++ src/controller/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const DesktopHomepage = () => {
+  return (
+    <div className="bg-white flex flex-row justify-center w-full">
+      <div className="bg-white [background:linear-gradient(180deg,rgb(28.34,43.29,103.06)_0%,rgb(76.4,22.06,112.62)_100%)] w-[1440px] h-[1024px] relative">
+        <div className="absolute w-[588px] h-[156px] -top-px left-[501px]">
+          <div className="absolute top-0 left-0 [text-shadow:9px_9px_0px_4px_#ffffff40] [background:linear-gradient(180deg,rgb(255,233.75,0)_0%,rgb(255,255,255)_100%)] [-webkit-background-clip:text] bg-clip-text [-webkit-text-fill-color:transparent] [text-fill-color:transparent] [font-family:'Moirai_One-Regular',Helvetica] font-normal text-transparent text-8xl tracking-[0] leading-[normal]">
+            StarXed
+          </div>
+          <p className="absolute top-[109px] left-[287px] [text-shadow:0px_4px_4px_#00000040] [background:linear-gradient(180deg,rgb(255,255,0)_0%,rgb(255,255,255)_56.5%)] [-webkit-background-clip:text] bg-clip-text [-webkit-text-fill-color:transparent] [text-fill-color:transparent] [font-family:'Sacramento-Regular',Helvetica] font-normal text-transparent text-[32px] text-center tracking-[0] leading-[normal]">
+            find the right star for you... ☆
+          </p>
+        </div>
+        <div className="absolute top-[303px] left-[501px] [text-shadow:0px_4px_4px_#00000040] [background:linear-gradient(180deg,rgb(255,255,0)_0%,rgb(255,255,255)_56.5%)] [-webkit-background-clip:text] bg-clip-text [-webkit-text-fill-color:transparent] [text-fill-color:transparent] [font-family:'Sacramento-Regular',Helvetica] font-normal text-transparent text-[32px] text-center tracking-[0] leading-[normal]">
+          Login
+        </div>
+        <div className="absolute top-[303px] left-[954px] [text-shadow:0px_4px_4px_#00000040] [background:linear-gradient(180deg,rgb(255,255,0)_0%,rgb(255,255,255)_56.5%)] [-webkit-background-clip:text] bg-clip-text [-webkit-text-fill-color:transparent] [text-fill-color:transparent] [font-family:'Sacramento-Regular',Helvetica] font-normal text-transparent text-[32px] text-center tracking-[0] leading-[normal]">
+          Register
+        </div>
+      </div>
+    </div>
+  );
+};
